7af76bc11acd5ceb124c89e05edc47c7ff4b6252
YamatoRyukyu <rightsidedog@gmail.com>
2025-11-30 16:28:36 +0900

    Player and Enemy

---
+++ Scenes/Mobs/mob.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+var player
+
+func _ready() -> void:
+	player = get_node("/root/Game/Player")
+
+func _physics_process(delta: float) -> void:
+	var speed = 150.0
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * speed
+	move_and_slide()
