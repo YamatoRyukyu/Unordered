45483cb31a852de3a32258c1c26d1602d633d325
YamatoRyukyu <rightsidedog@gmail.com>
2025-12-30 08:13:50 +0900

    [feature]Title Scene

---
+++ Scenes/title.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/survive_game.tscn")
+
+func _on_end_button_pressed() -> void:
+	get_tree().quit()
